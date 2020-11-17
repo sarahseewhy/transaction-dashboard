1be48e753b4c32e7208cb3da1d953b51ade39d61
Sarah C Y <sarahseewhy@gmail.com>
2020-11-17 20:13:45 +0000

    spike(app): authentication route
    * Try using Authentication Link from TrueLayer console.
    * I expect I will need to deconstruct the query parameters, this is a first iteration.

--- app/app.py
+++ app/app.py
@@ -1,13 +1,19 @@
 import os
 
-
 from flask import Flask
-
 
 app = Flask(__name__)
 
+CLIENT_ID = os.getenv('CLIENT_ID')
 
-@app.route('/')
-def hello_world():
-    print(os.getenv('CLIENT_ID'))
-    return 'Hello, World!'
+
+@app.route('/authenticate', methods=['GET'])
+def authenticate():
+    authentication_link = f'https://auth.truelayer-sandbox.com/?' \
+        f'response_type=code&' \
+        f'client_id={CLIENT_ID}' \
+        f'scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access' \
+        f'&redirect_uri=https://console.truelayer.com/redirect-page' \
+        f'&providers=uk-ob-all%20uk-oauth-all'
+
+    return f'<a href="{authentication_link}">Authenticate</a>'
