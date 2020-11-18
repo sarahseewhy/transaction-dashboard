f480512956d6031dfb10deadf38a8b285d4428ce
Sarah C Y <sarahseewhy@gmail.com>
2020-11-18 17:01:51 +0000

    spike(app): authentication callback route
    * Add initial authentication callback route which only prints message to browser on success.
    * Refactor REDIRECT_URI to ENV variables. Although not super sensitive it reduces duplication.
    * Reduce providers scope to only use mock banks.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 import os
 import urllib
 
-from flask import Flask
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
 CLIENT_ID = os.getenv('CLIENT_ID')
+SECRET_ID = os.getenv('SECRET_ID')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
 
 
 @app.route('/authenticate', methods=['GET'])
@@ -15,10 +18,27 @@ def authenticate():
         'response_mode': 'form_post',
         'client_id': CLIENT_ID,
         'scope': 'accounts transactions',
-        'redirect_uri': 'https://console.truelayer.com/redirect-page',
-        'providers': 'uk-ob-all uk-oauth-all uk-cs-mock'
+        'redirect_uri': REDIRECT_URI,
+        'providers': 'uk-cs-mock'
     })
 
     authentication_link = f'https://auth.truelayer-sandbox.com/?{url_parameters}'
 
     return f'<a href="{authentication_link}">Authenticate</a>'
+
+
+@app.route('/authenticate/callback', methods=['POST'])
+def authentication_handler():
+    authentication_code = request.form['code']
+
+    body = {
+        'client_id': CLIENT_ID,
+        'client_secret': SECRET_ID,
+        'code': authentication_code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': REDIRECT_URI,
+    }
+
+    response = requests.post('https://auth.truelayer-sandbox.com/connect/token', data=body)
+
+    return f'Authentication successful'
